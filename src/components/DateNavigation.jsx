f63b05075d4c92a0cdfd1921087557a4a349c875
Giammarco <giammarcogaudenzi@gmail.com>
2024-05-07 12:13:10 +0200

    clean

--- src/components/DateNavigation.jsx
+++ src/components/DateNavigation.jsx
@@ -1,17 +1,15 @@
 const DateNavigation = () => {
   const currentDate = new Date();
-
+  const formattedDate = currentDate.toLocaleDateString("it-IT", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   return (
     <div className="date-navigation">
       <button>prev</button>
-      <span>
-        {currentDate.toLocaleDateString("it-IT", {
-          weekday: "long",
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })}
-      </span>
+      <span>{formattedDate}</span>
       <button>next</button>
     </div>
   );
