e7b213d2ad6c024e1427e4b8d1df7940e7d2f412
Giammarco <giammarcogaudenzi@gmail.com>
2024-05-07 11:52:48 +0200

    sketch basic component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import "./App.css";
 function App() {
   return (
     <>
+      <div className="date-navigation">
+        <button>prev</button>
+        <span>current date</span>
+        <button>next</button>
+      </div>
+
       <h1>Hello world</h1>
     </>
   );
